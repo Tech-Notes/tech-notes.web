6519d9286ed492743165d933c85d88007bc56e4e
Thazin <thazinnaingmm@gmail.com>
2023-12-11 16:28:46 +0630

    fix: fixing side menuclick handler

--- src/components/side_bar/SideBar.jsx
+++ src/components/side_bar/SideBar.jsx
@@ -1,7 +1,11 @@
 import { faJs } from "@fortawesome/free-brands-svg-icons";
 import { faBox, faCartShopping, faDashboard, faRocket, faStar, faUser } from "@fortawesome/free-solid-svg-icons";
-import SideMenu from "./SideMenu";
-import { Div } from "../../base";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import {useNavigate} from "react-router";
+import {faAngleDown, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { Box , Div, Text} from '../../base';
+
 
 const sideMenuList = [
     {title: "Dashboard", icon: faDashboard, id: Math.random()*100, href: "/dashboard"},
@@ -13,10 +17,35 @@ const sideMenuList = [
 ]
 
 const SideBar = () => {
+
+  const navigate= useNavigate();
+
+  const [currentPath, setCurrentPath]=useState();
+
   return (
     <Div className="h-full p-2 border-r border-gray-200 dark:border-gray-600 cursor-pointer">
       {sideMenuList?.map((menu)=>{
-        return (<SideMenu key={menu.key} menu={menu}/>)
+        return(
+          <div key={menu.key}>
+            <Box onClick={()=>!menu.subMenu ? navigate(menu.href): setCurrentPath(prev=> prev === menu.href ? "" :menu.href)} 
+              className="flex h-14 items-center p-3 gap-3">
+              <FontAwesomeIcon icon={menu.icon} />
+              <Text>{menu.title}</Text>
+              {menu.subMenu && <div>{currentPath ? <FontAwesomeIcon icon={faAngleDown} /> : <FontAwesomeIcon icon={faAngleRight} />}</div> }
+            </Box>
+      
+            {currentPath === menu.href && 
+              menu.subMenu.map((subMenu)=>{
+                return(
+                  <Box key={subMenu.key} onClick={()=>navigate(subMenu.href)} className="flex h-13 items-center p-2 gap-3 ml-4" >
+                    <FontAwesomeIcon icon={subMenu.icon} />
+                    <Text>{subMenu.title}</Text>
+                  </Box>
+                )
+              })
+            }
+          </div>
+        )
       })}   
     </Div>
   );
