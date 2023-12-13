43aadd44d87602083eb507e4472c417a22971167
Thazin <thazinnaingmm@gmail.com>
2023-12-13 17:05:05 +0630

    fix: fix click handler with useCallback

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
-import { Link, LabeledInputController } from "../components/base";
+import { Link, LabeledInputController, Button } from "../components/base";
 import { useAuth } from "../provider/AuthProvider";
+import { useCallback } from "react";
 
 const Signin = () => {
 
@@ -24,6 +25,10 @@ const Signin = () => {
         navigate("/");
     }
 
+    const clickLinkHandler= useCallback(()=>{
+        navigate("/forgotpassword")
+    },[])
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col w-300 h-500 gap-2 ' autoComplete='off'>  
 
@@ -33,10 +38,12 @@ const Signin = () => {
                     
         <LabeledInputController control={control} name="password" label="Password" placeholder="Enter password..." type="password"/>
 
-        <Link onClick={()=>navigate("/forgotpassword")} className="text-xs cursor-pointer mt-1">Forgot password?</Link>
+        <Link onClick={clickLinkHandler} className="text-xs cursor-pointer mt-1">Forgot password?</Link>
         
-        <input type="submit" className="px-3 text-sm py-2 p-2 border border-pink-300 dark:border-gray-600 outline-none rounded-xl mt-3 cursor-pointer hover:bg-light-pink dark:hover:bg-gray-700 active:bg-pink-200  active:dark:bg-gray-800" 
-            value="Sign in"/>
+        <input type="submit" 
+            className="px-3 text-sm py-2 p-2 border border-pink-300 dark:border-gray-600 outline-none rounded-xl mt-3 cursor-pointer hover:bg-light-pink dark:hover:bg-gray-700 active:bg-pink-200  active:dark:bg-gray-800" 
+            value="Sign in"
+        />
     </form>
 
   );
