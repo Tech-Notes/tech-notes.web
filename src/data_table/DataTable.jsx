b4b1e37da6b19d61de7957fd42f86dd2f628c9d1
sai myo myat <saimyomyat511@gmail.com>
2023-12-19 00:30:06 +0630

    feat: added data table

---
+++ src/data_table/DataTable.jsx
@@ -0,0 +1,51 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './Table';
+
+const { useReactTable, flexRender } = require('@tanstack/react-table');
+
+const DataTable = ({ data, columns }) => {
+  const table = useReactTable({
+    data,
+    columns
+  });
+
+  return (
+    <Table>
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              return (
+                <TableHead key={header.id} className={header.column.columnDef.meta?.className}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.header, header.getContext())}
+                </TableHead>
+              );
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id} className={cell.column.columnDef.meta?.cellClass}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={columns.length} className="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default DataTable;
