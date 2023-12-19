70004b83a49328377eb7727a35d23b6d9606a3e2
sai myo myat <saimyomyat511@gmail.com>
2023-12-19 15:17:34 +0630

    fix: fixed table

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,17 +3,24 @@ import DataTable from '../data_table/DataTable';
 const Products = () => {
   return (
     <DataTable
-      data={[{ id: 1, name: 'mgmg' }]}
+      data={[
+        { id: 1, name: 'mg mg', age: 12 },
+        { id: 2, name: 'aye aye', age: 1 }
+      ]}
       columns={[
         {
+          id: 'select all',
+          header: ({ table }) => <div></div>
+        },
+        {
           accessorKey: 'name',
-          header: ({ table }) => <div aria-label="Name">Name</div>,
+          header: ({ columns }) => <div aria-label="Name">Name</div>,
           cell: ({ row }) => <div aria-label="Name">{row.getValue('name')}</div>
         },
         {
           accessorKey: 'age',
-          header: ({ table }) => <div aria-label="Age">Age</div>,
-          cell: ({ row }) => <div aria-label="Age">{row.getValue('name')}</div>
+          header: ({ columns }) => <div aria-label="Age">Age</div>,
+          cell: ({ row }) => <div aria-label="Age">{row.getValue('age')}</div>
         }
       ]}
     />
